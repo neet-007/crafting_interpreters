a15fac1e67049ec0f023fd17c928aa431b456eef
neet-007 <moayedeffect@gmail.com>
2024-11-28 20:39:19 +0300

    added clousers

--- c/debug.c
+++ c/debug.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "object.h"
 #include "value.h"
 #include "debug.h"
 
@@ -76,8 +76,15 @@ int disassembleInstruction(Chunk *chunk, int offset){
             return constantInstruction("OP_DEFINE_GLOBAL", chunk,
                                        offset);
         }
-        case OP_SET_GLOBAL:
+        case OP_SET_GLOBAL:{
             return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+        }
+        case OP_GET_UPVALUE:{
+            return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+        }
+        case OP_SET_UPVALUE:{
+            return byteInstruction("OP_SET_UPVALUE", chunk, offset);
+        }
         case OP_EQUAL:{
             return simpleInstruction("OP_EQUAL", offset);
         }
@@ -118,6 +125,26 @@ int disassembleInstruction(Chunk *chunk, int offset){
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         } case OP_CALL:{
             return byteInstruction("OP_CALL", chunk, offset);
+        }
+        case OP_CLOSURE: {
+            offset++;
+            uint8_t constant = chunk->code[offset++];
+            printf("%-16s %4d ", "OP_CLOSURE", constant);
+            printValue(chunk->constants.values[constant]);
+            printf("\n");
+
+            ObjFunction* function = AS_FUNCTION(chunk->constants.values[constant]);
+            for (int j = 0; j < function->upvalueCount; j++) {
+                int isLocal = chunk->code[offset++];
+                int index = chunk->code[offset++];
+                printf("%04d      |                     %s %d\n",
+                       offset - 2, isLocal ? "local" : "upvalue", index);
+            }
+
+            return offset;
+        }
+        case OP_CLOSE_UPVALUE:{
+            return simpleInstruction("OP_CLOSE_UPVALUE", offset);
         }
         case OP_RETURN:{
             return simpleInstruction("OP_RETURN", offset);
