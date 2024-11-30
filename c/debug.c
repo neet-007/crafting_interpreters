0423107a856441c8256a707425ecc1ea5fc4b5a3
neet-007 <moayedeffect@gmail.com>
2024-11-30 16:49:05 +0300

    added methods and initialzers

--- c/debug.c
+++ c/debug.c
@@ -37,6 +37,15 @@ static int constantInstruction(char *name, Chunk* chunk, int offset){
     return offset + 2;
 }
 
+static int invokeInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset){
     printf("%04d ", offset);
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
@@ -132,6 +141,9 @@ int disassembleInstruction(Chunk *chunk, int offset){
         } case OP_CALL:{
             return byteInstruction("OP_CALL", chunk, offset);
         }
+        case OP_INVOKE:{
+            return invokeInstruction("OP_INVOKE", chunk, offset);
+        }
         case OP_CLOSURE: {
             offset++;
             uint8_t constant = chunk->code[offset++];
@@ -157,6 +169,9 @@ int disassembleInstruction(Chunk *chunk, int offset){
         }
         case OP_CLASS:{
             return constantInstruction("OP_CLASS", chunk, offset);
+        }
+        case OP_METHOD:{
+            return constantInstruction("OP_METHOD", chunk, offset);
         }
         default:{
             printf("Unknown opcode %d\n", instruction);
